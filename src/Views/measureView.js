ed1657b3ff327d1f3e275c6a177976addee3e719
danirssx <rossdaniel5018@gmail.com>
2022-08-08 21:53:17 -0400

    functional F and C icon

--- src/Views/measureView.js
+++ src/Views/measureView.js
@@ -2,30 +2,46 @@ import View from "./View.js";
 import { state } from '../data.js'
 
 import cloud from 'url:../../icon white/cloudy.png'
-import celsius from 'url:../../icon white/celsius.png'
+import celsiusIcon from 'url:../../icon white/celsius.png'
+import fahrenheitIcon from 'url:../../icon white/fahrenheit.png'
 
 
 class measureView extends View {
     _parentEl = document.querySelector(".temp-container")
+    celsiusDOM = document.getElementById('btn-celsius')
+    fahrenheitDOM = document.getElementById('btn-fahrenheit')
 
-    _changeClass(old, newest) {
+    _changeClass(old, newest, render) {
       old.classList.remove('btn-measure-active');
       newest.classList.add('btn-measure-active')
 
-      old.addEventListener('click', function() {
-        if (old.classList.contains('btn-measure-active')) {
-          console.log(old)
-          this._changeClass(old, newest)
-        }
-      })
+      this.render()
+
+      if(!old.classList.contains('btn-measure-active')) return;
     }
 
     changeActive() {
-      let btnCelsius = document.getElementById('btn-celsius')
-      let btnFahrenheit = document.getElementById('btn-fahrenheit')
+      let btnCelsius = this.celsiusDOM;
+      let btnFahrenheit = this.fahrenheitDOM;
 
       btnCelsius.addEventListener('click', this._changeClass.bind(this, btnFahrenheit, btnCelsius))
       btnFahrenheit.addEventListener('click', this._changeClass.bind(this, btnCelsius, btnFahrenheit))
+    }
+
+    _changeMeasure() {
+      if(this.celsiusDOM.classList.contains('btn-measure-active')) {
+        return state.weather.celsius
+      } else {
+        return state.weather.fahrenheit
+      }
+    }
+
+    _changeIcon() {
+      if(this.celsiusDOM.classList.contains('btn-measure-active')) {
+        return celsiusIcon
+      } else {
+        return fahrenheitIcon
+      }
     }
 
     _generateMarkup() {
@@ -38,8 +54,8 @@ class measureView extends View {
         />
       </div>
       <div class="num-temp">
-        <h1 class="temperature">${Math.round(state.weather.celsius)}</h1>
-        <img src="${celsius}" alt="f" class="measure" />
+        <h1 class="temperature">${Math.round(this._changeMeasure())}</h1>
+        <img src="${this._changeIcon()}" alt="f" class="measure" />
       </div>`
     }
 }
